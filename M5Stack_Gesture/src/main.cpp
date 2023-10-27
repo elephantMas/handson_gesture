18f0d8a0654501b06dd035387ed1600be81f1859
elephantMas <elephant.mas@gmail.com>
2023-10-28 06:23:26 +0900

    fix

--- M5Stack_Gesture/src/main.cpp
+++ M5Stack_Gesture/src/main.cpp
@@ -1,29 +1,21 @@
 #include <Arduino.h>
 #include <M5Stack.h>
-#include <paj7620.h>
-#include <Gesture.h>
+#include <DFRobot_PAJ7620U2.h>
 
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
 // Gesture
-#define GES_REACTION_TIME 500
-#define GES_ENTRY_TIME 800
-#define GES_QUIT_TIME 1000
-
-paj7620 Gesture;
+DFRobot_PAJ7620U2 sensor;
 
 // WiFi credentials.
-const char *WIFI_SSID = "Buffalo-G-918E";
-const char *WIFI_PASS = "e45kixcxtkred421221214";
-
-#include "esp_wpa2.h" //wpa2 library for connections to Enterprise networks
-// #define EAP_IDENTITY "20FI094"                 // 学籍番号
-// #define EAP_USERNAME "20FI094"                 // 学籍番号
-// #define EAP_PASSWORD "Que_sera7760"            // 大学パスワード
-// const char *ssid = "TDU_MRCL_WLAN_DOT1X_2.4G"; // SSID
-int counter = 0; // タイムアウト監視用
+#include "esp_wpa2.h"                          //wpa2 library for connections to Enterprise networks
+#define EAP_IDENTITY "20FI094"                 // 学籍番号
+#define EAP_USERNAME "20FI094"                 // 学籍番号
+#define EAP_PASSWORD "Que_sera7760"            // 大学パスワード
+const char *ssid = "TDU_MRCL_WLAN_DOT1X_2.4G"; // SSID
+int counter = 0;                               // タイムアウト監視用
 
 // MQTT credentials.
 const char *brokerAdr = "test.mosquitto.org";
@@ -50,11 +42,12 @@ void setup()
   delay(5000);
 
   // Gesture initialize
-  if (Gesture.init())
+  while (sensor.begin() != 0)
   {
-    Serial.println("PAJ7620U2 initialization failed");
+    Serial.printf("initial PAJ7620U2 failure!");
+    delay(500);
   }
-  Serial.println("Please input your gestures:\n");
+  sensor.setGestureHighRate(true);
 
   // wifi connect
   M5.Lcd.fillScreen(WHITE);
@@ -64,8 +57,8 @@ void setup()
   WiFi.disconnect(true); // disconnect form wifi to set new wifi connection
   WiFi.mode(WIFI_STA);   // init wifi mode
 
-  // WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_IDENTITY, EAP_USERNAME, EAP_PASSWORD);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_IDENTITY, EAP_USERNAME, EAP_PASSWORD);
+  // WiFi.begin(WIFI_SSID, WIFI_PASS);
   delay(1000);
 
   // 接続できなかった際のタイムアウト処理
@@ -98,61 +91,17 @@ void loop()
   M5.Lcd.setCursor(0, 0);
   M5.Lcd.print("Make Some Gesture");
 
-  paj7620_gesture_t result;
-  if (Gesture.getResult(result))
+  DFRobot_PAJ7620U2::eGesture_t gesture = sensor.getGesture();
+  if (gesture != sensor.eGestureNone)
   {
-    switch (result)
-    {
-    case UP:
-      delay(GES_ENTRY_TIME);
-      mqttClient.publish(topic, "UP");
-      delay(GES_QUIT_TIME);
-      break;
-    case DOWN:
-      delay(GES_ENTRY_TIME);
-      mqttClient.publish(topic, "DOWN");
-      delay(GES_QUIT_TIME);
-      break;
-    case LEFT:
-      delay(GES_ENTRY_TIME);
-      mqttClient.publish(topic, "LEFT");
-      delay(GES_QUIT_TIME);
-      break;
-    case RIGHT:
-      delay(GES_ENTRY_TIME);
-      mqttClient.publish(topic, "RIGHT");
-      delay(GES_QUIT_TIME);
-      break;
-    case PUSH:
-      delay(GES_ENTRY_TIME);
-      mqttClient.publish(topic, "PUSH");
-      delay(GES_QUIT_TIME);
-      break;
-    case POLL:
-      delay(GES_ENTRY_TIME);
-      mqttClient.publish(topic, "POLL");
-      delay(GES_QUIT_TIME);
-      break;
-    case CLOCKWISE:
-      delay(GES_ENTRY_TIME);
-      mqttClient.publish(topic, "CLOCKWISE");
-      delay(GES_QUIT_TIME);
-      break;
-    case ANTI_CLOCKWISE:
-      delay(GES_ENTRY_TIME);
-      mqttClient.publish(topic, "ANTI_CLOCKWISE");
-      delay(GES_QUIT_TIME);
-      break;
-    case WAVE:
-      delay(GES_ENTRY_TIME);
-      mqttClient.publish(topic, "WAVE");
-      delay(GES_QUIT_TIME);
-      break;
-    default:
-      break;
-    }
+    String result = sensor.gestureDescription(gesture);
+    mqttClient.publish(topic, result.c_str());
+    M5.Lcd.fillScreen(WHITE);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.print(result);
   }
-  delay(100);
+
+  delay(5000);
 }
 
 void reconnect()
--- front/next.config.js
+++ front/next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eactStrictMode: true,
-};
+const nextConfig = {}
 
-module.exports = {
-  distDir: "build",
-  ...nextConfig,
-};
+module.exports = nextConfig
