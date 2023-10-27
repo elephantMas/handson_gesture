7d85e6a69ad7928c6e9f6ef2c2a70b915c0577b8
elephantMas <elephant.mas@gmail.com>
2023-10-27 21:05:05 +0900

    handson

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = {
+  distDir: "build",
+  ...nextConfig,
+};
